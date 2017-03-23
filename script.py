ff2be3f3f533ac57fa5389b89fe4f46ba11cd160
Leon Eckert <eckertleon@gmail.com>
2017-03-23 11:35:19 -0400

    local server shift

--- script.py
+++ script.py
@@ -25,7 +25,7 @@ all_paths = {}
 
 # copy the input image into the local project folder:
 path_local_original = os.path.join(project_path, "original.jpg")
-shutil.copy(path, path_local_orig)
+shutil.copy(path, path_local_orignal)
 
 all_paths["local_original"] = path_local_original
 
