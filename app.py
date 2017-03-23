53693b34bde6d2f57bffc2309ea9677f4ce4baf8
Leon Eckert <eckertleon@gmail.com>
2017-03-23 15:18:48 -0400

    local server shift

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '.'
+UPLOAD_FOLDER = 'static'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
